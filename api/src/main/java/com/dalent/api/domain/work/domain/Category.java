07b260e17bed10c8eb0c49551febce349be8d618
huewilliams <4879sinwoo@naver.com>
2020-06-26 11:36:59 +0900

    DAL-7 [ADD] category enum class

---
+++ api/src/main/java/com/dalent/api/domain/work/domain/Category.java
@@ -0,0 +1,17 @@
+package com.dalent.api.domain.work.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Category {
+
+    ART("ART", "미술"),
+    MUSIC("MUSIC", "음악"),
+    PROGRAMMING("PROGRAMMING", "프로그래밍");
+
+    private final String key;
+    private final String value;
+
+}
