0eba02b6db7e37cb07cc0f1d06321981acfcf73e
huewilliams <4879sinwoo@naver.com>
2020-07-09 22:53:22 +0900

    DAL-8 [ADD] findByNickname method

--- api/src/main/java/com/dalent/api/domain/user/dao/UserRepository.java
+++ api/src/main/java/com/dalent/api/domain/user/dao/UserRepository.java
@@ -3,5 +3,9 @@ package com.dalent.api.domain.user.dao;
 import com.dalent.api.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByNickname(String nickname);
 }
