74f9240e277664c527832b4b49e7f02aa7dc8e60
huewilliams <4879sinwoo@naver.com>
2020-07-09 23:17:40 +0900

    DAL-8 [ADD] invalid token error code

--- api/src/main/java/com/dalent/api/global/error/exception/ErrorCode.java
+++ api/src/main/java/com/dalent/api/global/error/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum ErrorCode {
     // User
     USER_NOT_FOUND(400, "U001", "User Not Found"),
-    USER_DUPLICATE(409, "U002", "User Duplicated");
+    USER_DUPLICATE(409, "U002", "User Duplicated"),
+
+    //Auth
+    INVALID_TOKEN(401, "A001", "Invalid Token");
 
     private final int status;
     private final String code;
