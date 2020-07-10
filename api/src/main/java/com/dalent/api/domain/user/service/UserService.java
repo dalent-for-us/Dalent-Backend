ac7d78aaf0660a6cc6c45002c8b1d00079dc109b
huewilliams <4879sinwoo@naver.com>
2020-07-10 14:09:02 +0900

    DAL-10 [ADD] user service, getMyInfo method

--- api/src/main/java/com/dalent/api/domain/user/service/UserService.java
+++ api/src/main/java/com/dalent/api/domain/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.dalent.api.domain.user.service;
 
+import com.dalent.api.domain.auth.exception.UserNotFoundException;
 import com.dalent.api.domain.user.dao.UserRepository;
 import com.dalent.api.domain.user.domain.User;
+import com.dalent.api.domain.user.dto.UserInfoResponseDto;
 import com.dalent.api.domain.user.dto.UserJoinRequestDto;
 import com.dalent.api.domain.user.exception.UserDuplicateException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +30,23 @@ public class UserService {
                         .password(password)
                         .build()
         );
+    }
+
+    public UserInfoResponseDto getMyInfo() {
+        String nickname = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return userRepository.findByNickname(nickname)
+                .map(user -> {
+                    Long gallery = (user.getGallery() != null) ? user.getGallery().getGalleryId() : null;
+
+                    return UserInfoResponseDto.builder()
+                            .id(user.getId())
+                            .nickname(user.getNickname())
+                            .introduce(user.getIntroduce())
+                            .profile_image(user.getProfile_image())
+                            .gallery(gallery)
+                            .build();
+                })
+                .orElseThrow(UserNotFoundException::new);
     }
 }
