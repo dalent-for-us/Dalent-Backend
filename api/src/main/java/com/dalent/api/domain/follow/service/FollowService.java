2bca3ea359af96c8f8ea4c61dd831cffc1f8224a
huewilliams <4879sinwoo@naver.com>
2020-07-13 01:23:45 +0900

    DAL-11 [ADD] follow service, follow method

---
+++ api/src/main/java/com/dalent/api/domain/follow/service/FollowService.java
@@ -0,0 +1,30 @@
+package com.dalent.api.domain.follow.service;
+
+import com.dalent.api.domain.auth.exception.UserNotFoundException;
+import com.dalent.api.domain.follow.dao.FollowRepository;
+import com.dalent.api.domain.follow.domain.Follow;
+import com.dalent.api.domain.follow.dto.FollowRequestDto;
+import com.dalent.api.domain.user.dao.UserRepository;
+import com.dalent.api.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class FollowService {
+
+    private final FollowRepository followRepository;
+
+    private final UserRepository userRepository;
+
+    public void follow(FollowRequestDto request) {
+        User user = userRepository.findByNickname(request.getUser_nickname()).orElseThrow(UserNotFoundException::new);
+        User target = userRepository.findByNickname(request.getTarget_nickname())
+                .orElseThrow(UserNotFoundException::new);
+
+        followRepository.save(Follow.builder()
+                .user(user)
+                .target(target)
+                .build());
+    }
+}
