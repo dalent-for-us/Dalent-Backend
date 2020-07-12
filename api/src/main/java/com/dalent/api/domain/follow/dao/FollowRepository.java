0dd59f3c1cb5f334ef3af083dda8f94b2eb46cc9
huewilliams <4879sinwoo@naver.com>
2020-07-13 02:04:25 +0900

    DAL-11 [ADD] findAllByUser/Target method

--- api/src/main/java/com/dalent/api/domain/follow/dao/FollowRepository.java
+++ api/src/main/java/com/dalent/api/domain/follow/dao/FollowRepository.java
@@ -1,7 +1,13 @@
 package com.dalent.api.domain.follow.dao;
 
 import com.dalent.api.domain.follow.domain.Follow;
+import com.dalent.api.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    List<Follow> findAllByUser(User user);
+    List<Follow> findAllByTarget(User user);
 }
