9424ed950bf06a5827aa29963b357e38d1b43534
huewilliams <4879sinwoo@naver.com>
2020-07-13 01:24:21 +0900

    DAL-11 [ADD] follow cotroller, POST/follow API

---
+++ api/src/main/java/com/dalent/api/domain/follow/controller/FollowController.java
@@ -0,0 +1,21 @@
+package com.dalent.api.domain.follow.controller;
+
+import com.dalent.api.domain.follow.dto.FollowRequestDto;
+import com.dalent.api.domain.follow.service.FollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/follow")
+@RestController
+public class FollowController {
+
+    private final FollowService followService;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void followUser(@RequestBody FollowRequestDto requestDto) {
+        followService.follow(requestDto);
+    }
+}
