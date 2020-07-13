71423af8688cb63401781e45c1ca89814d4647df
huewilliams <4879sinwoo@naver.com>
2020-07-13 18:35:17 +0900

    DAL-14 [ADD] findByCategoryOrderByStarsDesc method

--- api/src/main/java/com/dalent/api/domain/work/dao/WorkRepository.java
+++ api/src/main/java/com/dalent/api/domain/work/dao/WorkRepository.java
@@ -1,8 +1,12 @@
 package com.dalent.api.domain.work.dao;
 
+import com.dalent.api.domain.work.domain.Category;
 import com.dalent.api.domain.work.domain.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkRepository extends JpaRepository<Work, Long> {
 
+    List<Work> findByCategoryOrderByStarsDesc(Category category);
 }
