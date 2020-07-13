b16e834eca41c025f591ee7b82e9e3d6d7a55b53
huewilliams <4879sinwoo@naver.com>
2020-07-14 08:39:43 +0900

    DAL-16 [ADD] findByUser method

--- api/src/main/java/com/dalent/api/domain/star/dao/StarRepository.java
+++ api/src/main/java/com/dalent/api/domain/star/dao/StarRepository.java
@@ -1,11 +1,14 @@
 package com.dalent.api.domain.star.dao;
 
 import com.dalent.api.domain.star.domain.Star;
+import com.dalent.api.domain.user.domain.User;
 import com.dalent.api.domain.work.domain.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StarRepository extends JpaRepository<Star, Long> {
     List<Star> findAllByWork(Work work);
+    Optional<Star> findByUser(User user);
 }
