ce764816a9f99835035047e8a5c9faa268311d4c
huewilliams <4879sinwoo@naver.com>
2020-07-13 22:06:37 +0900

    DAL-15 [ADD] PATCH/comments/{comment_id} API

--- api/src/main/java/com/dalent/api/domain/comment/controller/CommentController.java
+++ api/src/main/java/com/dalent/api/domain/comment/controller/CommentController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/comments")
@@ -25,5 +27,14 @@ public class CommentController {
     @GetMapping
     public List<CommentResponseDto> getComments(@RequestParam String work_id) {
         return commentService.getComments(work_id);
+    }
+
+    @PatchMapping("/{comment_id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @Transactional
+    public void reviseComment(@PathVariable("comment_id") String commentId,
+                              @RequestBody Map<String, String> content) {
+
+        commentService.reviseComment(commentId, content.get("content"));
     }
 }
