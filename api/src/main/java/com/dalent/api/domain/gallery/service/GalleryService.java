26f5ea1727fb98a15b287d18e76730bc85b14636
huewilliams <4879sinwoo@naver.com>
2020-07-13 15:23:56 +0900

    DAL-13 [ADD] updateBannerImage method

--- api/src/main/java/com/dalent/api/domain/gallery/service/GalleryService.java
+++ api/src/main/java/com/dalent/api/domain/gallery/service/GalleryService.java
@@ -5,6 +5,7 @@ import com.dalent.api.domain.gallery.dao.GalleryRepository;
 import com.dalent.api.domain.gallery.domain.Gallery;
 import com.dalent.api.domain.gallery.dto.GalleryInfoResponseDto;
 import com.dalent.api.domain.gallery.exception.GalleryNotFoundException;
+import com.dalent.api.domain.gallery.exception.NotMyGalleryException;
 import com.dalent.api.domain.user.dao.UserRepository;
 import com.dalent.api.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -21,11 +22,9 @@ public class GalleryService {
 
     public GalleryInfoResponseDto getGallery(String nickname) {
         User user = userRepository.findByNickname(nickname).orElseThrow(UserNotFoundException::new);
-        Gallery gallery;
-        if(!galleryRepository.findByUser(user).isPresent()) {
-            this.makeNewGallery(user);
-        }
-        gallery = galleryRepository.findByUser(user).orElseThrow(GalleryNotFoundException::new);
+        this.makeNewGallery(user);
+        Gallery gallery = galleryRepository.findByUser(user).orElseThrow(GalleryNotFoundException::new);
+
         return GalleryInfoResponseDto.builder()
                 .gallery_id(gallery.getGalleryId())
                 .banner_image(gallery.getBannerImage())
@@ -33,6 +32,17 @@ public class GalleryService {
     }
 
     private void makeNewGallery(User user) {
-        galleryRepository.save(Gallery.builder().user(user).build());
+        if(!galleryRepository.findByUser(user).isPresent())
+            galleryRepository.save(Gallery.builder().user(user).build());
+    }
+
+    public void updateBannerImage(String nickname, String bannerImage) {
+        String clientName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if(!clientName.equals(nickname)) throw new NotMyGalleryException();
+        User user = userRepository.findByNickname(nickname).orElseThrow(UserNotFoundException::new);
+        this.makeNewGallery(user);
+        Gallery gallery = galleryRepository.findByUser(user).orElseThrow(GalleryNotFoundException::new);
+        gallery.changeBannerImage(bannerImage);
+        galleryRepository.save(gallery);
     }
 }
