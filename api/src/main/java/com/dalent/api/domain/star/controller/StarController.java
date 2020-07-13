20ac23992ea30f9a2be23956fadf0dd0e0e56eef
huewilliams <4879sinwoo@naver.com>
2020-07-14 08:29:34 +0900

    DAL-16 [ADD] GET/stars/{work_id} API

--- api/src/main/java/com/dalent/api/domain/star/controller/StarController.java
+++ api/src/main/java/com/dalent/api/domain/star/controller/StarController.java
@@ -1,13 +1,10 @@
 package com.dalent.api.domain.star.controller;
 
-import com.dalent.api.domain.star.dto.GiveStarRequestDto;
 import com.dalent.api.domain.star.service.StarService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -20,5 +17,10 @@ public class StarController {
     @PostMapping
     public long giveStar(@RequestBody Map<String, String> work_id) {
         return starService.giveStar(work_id.get("work_id"));
+    }
+
+    @GetMapping("/{work_id}")
+    public List<String> getStars(@PathVariable("work_id") String workId) {
+        return starService.getStars(workId);
     }
 }
