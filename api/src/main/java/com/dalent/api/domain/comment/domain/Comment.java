58f5d6c9b1f0383adf2147924f853320c922fb2b
huewilliams <4879sinwoo@naver.com>
2020-07-13 20:19:31 +0900

    DAL-15 [FIX] comment mapping and builder

--- api/src/main/java/com/dalent/api/domain/comment/domain/Comment.java
+++ api/src/main/java/com/dalent/api/domain/comment/domain/Comment.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -28,15 +30,20 @@ public class Comment {
 
     private String content;
 
-    @OneToOne
-    @JoinColumn(name = "child_comment")
-    private Comment childComment;
+    @OneToMany(mappedBy = "parent_comment")
+    private List<Comment> childComment;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_comment")
+    private Comment parent_comment;
 
     @Builder
-    public Comment(Work work, User writer, String content) {
+    public Comment(Work work, User writer, String content, Comment parentComment) {
         this.work = work;
         this.writer = writer;
         this.content = content;
+        this.childComment = new ArrayList<>();
+        this.parent_comment = parentComment;
     }
 
 }
