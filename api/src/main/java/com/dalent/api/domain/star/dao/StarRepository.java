c019f59dce4263fe60372b0eddf7a73de51b4382
huewilliams <4879sinwoo@naver.com>
2020-07-14 08:27:45 +0900

    DAL-16 [ADD] findAllByWork method

--- api/src/main/java/com/dalent/api/domain/star/dao/StarRepository.java
+++ api/src/main/java/com/dalent/api/domain/star/dao/StarRepository.java
@@ -1,7 +1,11 @@
 package com.dalent.api.domain.star.dao;
 
 import com.dalent.api.domain.star.domain.Star;
+import com.dalent.api.domain.work.domain.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StarRepository extends JpaRepository<Star, Long> {
+    List<Star> findAllByWork(Work work);
 }
