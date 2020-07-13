cd06e7fc8049eeb2af978e1868caa22439696d10
huewilliams <4879sinwoo@naver.com>
2020-07-13 14:30:03 +0900

    DAL-13 [ADD] findByUser method

--- api/src/main/java/com/dalent/api/domain/gallery/dao/GalleryRepository.java
+++ api/src/main/java/com/dalent/api/domain/gallery/dao/GalleryRepository.java
@@ -1,7 +1,12 @@
 package com.dalent.api.domain.gallery.dao;
 
 import com.dalent.api.domain.gallery.domain.Gallery;
+import com.dalent.api.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
+
+    Optional<Gallery> findByUser(User user);
 }
