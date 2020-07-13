b1339737c4ef68d52b13b9b50743575bd08312f1
huewilliams <4879sinwoo@naver.com>
2020-07-13 21:55:01 +0900

    DAL-15 [ADD] GET/comments API

--- api/src/main/java/com/dalent/api/domain/comment/controller/CommentController.java
+++ api/src/main/java/com/dalent/api/domain/comment/controller/CommentController.java
@@ -1,10 +1,13 @@
 package com.dalent.api.domain.comment.controller;
 
+import com.dalent.api.domain.comment.dto.CommentResponseDto;
 import com.dalent.api.domain.comment.dto.CreateCommentRequestDto;
 import com.dalent.api.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/comments")
@@ -17,5 +20,10 @@ public class CommentController {
     @ResponseStatus(value = HttpStatus.CREATED)
     public long createComment(@RequestBody CreateCommentRequestDto requestDto) {
         return commentService.createComment(requestDto);
+    }
+
+    @GetMapping
+    public List<CommentResponseDto> getComments(@RequestParam String work_id) {
+        return commentService.getComments(work_id);
     }
 }
