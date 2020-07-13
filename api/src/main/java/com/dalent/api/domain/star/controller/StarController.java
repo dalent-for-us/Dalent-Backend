e851041dfeae23de48bc5484d63562c2470f9b28
huewilliams <4879sinwoo@naver.com>
2020-07-14 08:47:33 +0900

    DAL-16 [ADD] DELETE/stars/{work_id} API

--- api/src/main/java/com/dalent/api/domain/star/controller/StarController.java
+++ api/src/main/java/com/dalent/api/domain/star/controller/StarController.java
@@ -2,8 +2,10 @@ package com.dalent.api.domain.star.controller;
 
 import com.dalent.api.domain.star.service.StarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 
@@ -22,5 +24,12 @@ public class StarController {
     @GetMapping("/{work_id}")
     public List<String> getStars(@PathVariable("work_id") String workId) {
         return starService.getStars(workId);
+    }
+
+    @DeleteMapping("/{work_id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @Transactional
+    public void cancelStar(@PathVariable("work_id") String workId) {
+        starService.cancelStar(workId);
     }
 }
