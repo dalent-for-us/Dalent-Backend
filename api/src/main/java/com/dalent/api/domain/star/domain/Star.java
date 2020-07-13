e998d5b5b3f0ea8b1daafe6aa69cb4dd386a1f59
huewilliams <4879sinwoo@naver.com>
2020-07-13 23:43:44 +0900

    DAL-16 [ADD] Star entity

---
+++ api/src/main/java/com/dalent/api/domain/star/domain/Star.java
@@ -0,0 +1,43 @@
+package com.dalent.api.domain.star.domain;
+
+import com.dalent.api.domain.user.domain.User;
+import com.dalent.api.domain.work.domain.Category;
+import com.dalent.api.domain.work.domain.Work;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Star {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long starId;
+
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "work_id")
+    private Work work;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "target_id")
+    private User target;
+
+    @Builder
+    public Star(Category category, Work work, User user, User target) {
+        this.category = category;
+        this.work = work;
+        this.user = user;
+        this.target = target;
+    }
+}
