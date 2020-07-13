30a59157a563309ccebd7133f3ecb60358fee088
huewilliams <4879sinwoo@naver.com>
2020-07-13 16:48:52 +0900

    DAL-14 [ADD] GET/works/{work_id} API

--- api/src/main/java/com/dalent/api/domain/work/controller/WorkController.java
+++ api/src/main/java/com/dalent/api/domain/work/controller/WorkController.java
@@ -1,6 +1,7 @@
 package com.dalent.api.domain.work.controller;
 
 import com.dalent.api.domain.work.dto.CreateWorkRequestDto;
+import com.dalent.api.domain.work.dto.WorkDetailResponseDto;
 import com.dalent.api.domain.work.service.WorkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,5 +18,10 @@ public class WorkController {
     @ResponseStatus(HttpStatus.CREATED)
     public long createWork(@RequestBody CreateWorkRequestDto requestDto) {
         return workService.createWork(requestDto);
+    }
+
+    @GetMapping("/{work_id}")
+    public WorkDetailResponseDto findWork(@PathVariable("work_id") String workId) {
+        return workService.findWork(Long.parseLong(workId));
     }
 }
