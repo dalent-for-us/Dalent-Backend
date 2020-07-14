10f2b316513f906cd3f2fd7ce6885f7acc793c71
huewilliams <4879sinwoo@naver.com>
2020-07-14 10:39:17 +0900

    DAL-16 [ADD] findTop10 Users methods

--- api/src/main/java/com/dalent/api/domain/user/dao/UserRepository.java
+++ api/src/main/java/com/dalent/api/domain/user/dao/UserRepository.java
@@ -3,9 +3,14 @@ package com.dalent.api.domain.user.dao;
 import com.dalent.api.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByNickname(String nickname);
+    List<User> findDistinctTop10ByStarsNotNullOrderByArtStarCountsDesc();
+    List<User> findDistinctTop10ByStarsNotNullOrderByMusicStarCountsDesc();
+    List<User> findDistinctTop10ByStarsNotNullOrderByFashionStarCountsDesc();
+    List<User> findDistinctTop10ByStarsNotNullOrderByProgrammingStarCountsDesc();
 }
