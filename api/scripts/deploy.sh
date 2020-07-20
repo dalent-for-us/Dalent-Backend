7b9ec36936d5cefb0ed3dad088003dfbf3a3715d
huewilliams <4879sinwoo@naver.com>
2020-07-20 09:52:11 +0900

    DAL-17 [ADD] deploy.sh

---
+++ api/scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/dalent
+PROJECT_NAME=dalent
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -fl dalent | grep jar | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케아션 pid : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name : $JAR_NAME"
+
+echo "> JAR_NAME 에 실행권한 추가"
+chomod +x $JAR_NAME
+chomod u+w+x $REPOSITORY/nohup.out
+
+echo "> $JAR_NAME 실행"
+nohup java -jar \ 
+    -Dspring.config.location=classpath:/application.properties, application-secret.properties \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
